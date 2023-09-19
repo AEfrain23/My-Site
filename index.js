799eac473debab821364b2356fc85f2ccc1bad24
Angel Puruncajas Guevara <angelefrain23@gmail.com>
2023-09-20 00:35:10 +0100

    Updated navbar for mobile functionality

---
+++ index.js
@@ -0,0 +1,14 @@
+const navbarButton = document.querySelector("navbar");
+const navbarToggle = document.querySelector("mobile-navbar-toggle");
+
+document.querySelector("button").addEventListener("click", function () {
+    document.querySelector("#navbar").classList.toggle("hide");
+});
+
+$(".btn").click(function () {
+    document.querySelector("#navbar").classList.toggle("hide");
+});
+
+
+
+// IMAGE SELECTION FOR ALBUM SECTION:
